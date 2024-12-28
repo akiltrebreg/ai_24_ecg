9f39cbc17507fb31167e8b345e8fb2e6df305cc1
akiltrebreg <akiltrebreg@yandex.ru>
2024-12-28 14:19:35 +0300

    Add logging to frontend

--- frontend/app.py
+++ frontend/app.py
@@ -5,12 +5,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import os
+import logging
+from logger_config import logger
 
 # URL backend
 BACKEND_URL = "http://backend:8000"
 
 # Заголовок приложения
 st.title("Приложение для анализа сигналов ЭКГ и определения состояния здоровья человека")
+logger.info("Приложение запущено")
 
 # Сохранение состояния загруженного файла для обучения
 if "dataset_name" not in st.session_state:
@@ -25,19 +28,24 @@ if "top20_diseases" not in st.session_state:
     st.session_state["top20_diseases"] = None
 if "top15_diseases" not in st.session_state:
     st.session_state["top15_diseases"] = None
+if "dataset_name_prediction" not in st.session_state:
+    st.session_state["dataset_name_prediction"] = None
 
 
 # Part 1: Загрузка файла
 uploaded_file = st.file_uploader("Загрузите ZIP-файл для обучения", type=["zip"])
 if uploaded_file is not None and st.session_state["dataset_name"] is None:
+    logger.info("Файл загружен пользователем: %s", uploaded_file.name)
     # Отправляем файл на сервер только один раз
     files = {"file": (uploaded_file.name, uploaded_file.read(), "application/zip")}
     upload_response = requests.post(f"{BACKEND_URL}/upload_dataset", files=files)
 
     if upload_response.status_code == 200:
+        logger.info("Файл успешно отправлен на сервер: %s", uploaded_file.name)
         st.success("Файл успешно загружен!")
         st.session_state["dataset_name"] = upload_response.json()["filepath"].split("/")[-1]  # Сохраняем имя файла
     else:
+        logger.error("Ошибка загрузки файла на сервер: %s", upload_response.text)
         st.error(f"Ошибка загрузки файла: {upload_response.text}")
 
 # Part 2: EDA
@@ -52,12 +60,14 @@ if st.session_state["dataset_name"]:
     # Отображение кнопки только после загрузки файла
     if not st.session_state["eda_performed"]:
         if st.button("Провести EDA"):
+            logger.info("Пользователь инициировал выполнение EDA для файла: %s", dataset_name)
             with st.spinner("Обработка данных, пожалуйста, подождите..."):
                 response = requests.get(
                     f"{BACKEND_URL}/get_eda_info",
                     data={"dataset_name": st.session_state["dataset_name"].replace(".zip", "")}
                 )
                 if response.status_code == 200:
+                    logger.info("EDA успешно выполнен для файла: %s", dataset_name)
                     result = response.json()
 
                     # Сохраняем данные в session_state
@@ -68,103 +78,14 @@ if st.session_state["dataset_name"]:
                     st.session_state["eda_performed"] = True
                     st.success("EDA успешно выполнен!")
                 else:
+                    logger.error("Ошибка выполнения EDA: %s", response.text)
                     st.error(f"Ошибка: {response.status_code} — {response.text}")
     else:
+        logger.info("EDA уже выполнен для файла: %s", dataset_name)
         st.success("EDA уже выполнен.")
 else:
+    logger.warning("Попытка выполнения EDA без загрузки файла")
     st.info("Пожалуйста, загрузите файл, чтобы произвести анализ.")
-
-
-# Проверяем, загружены ли данные
-if st.session_state["df_exploded"] is not None and st.session_state["df3"] is not None\
-    and st.session_state["top20_diseases"] is not None and st.session_state["top15_diseases"] is not None:
-
-    df_exploded = st.session_state["df_exploded"]
-    df3 = st.session_state["df3"]
-    top20_diseases = st.session_state["top20_diseases"]
-    top15_diseases = st.session_state["top15_diseases"]
-
-    # Отображение описательной статистики
-    st.markdown("Описательная статистика:")
-    st.write(df_exploded.describe())
-
-    # Построение гистограммы
-    fig1, ax1 = plt.subplots(figsize=(10, 5))
-    sns.histplot(df3['len_disease'], kde=True, ax=ax1)
-    ax1.set_xlabel('Количество заболеваний')
-    ax1.set_ylabel('Количество пациентов')
-    ax1.set_title('Гистограмма распределения количества заболеваний у одного пациента')
-    st.pyplot(fig1)
-
-    # Группировка данных
-    st.write('Количество женщин и мужчин среди пациентов:')
-    st.write(df_exploded.groupby('gender').agg({'id': 'nunique'}))
-
-    # Построение ещё одной гистограммы
-    fig2, ax2 = plt.subplots(figsize=(10, 5))
-    sns.histplot(data=df_exploded, x='age', hue='gender', kde=True, ax=ax2)
-    ax2.set_xlabel('Возраст')
-    ax2.set_ylabel('Количество пациентов')
-    ax2.set_title('Гистограмма распределения возраста')
-    st.pyplot(fig2)
-
-
-
-    # Интерфейс для выбора заболевания и пола
-    selected_disease = st.selectbox("Выберите заболевание", top20_diseases)
-    gender_filter = st.radio("Выберите пол", ["Оба", "Мужчины", "Женщины"])
-
-    # Фильтрация данных
-    @st.cache_data
-    def filter_data(disease, gender_filter, df):
-        df_filtered = df[df.disease_name == disease]
-        if gender_filter != "Оба":
-            gender = "Male" if gender_filter == "Мужчины" else "Female"
-            df_filtered = df_filtered[df_filtered.gender == gender]
-        return df_filtered
-
-    df_to_plot = filter_data(selected_disease, gender_filter, df_exploded)
-
-    # Построение графика
-    fig3, ax3 = plt.subplots(figsize=(10, 5))
-    sns.histplot(data=df_to_plot, x='age', kde=True, ax=ax3)
-    ax3.set_xlabel('Возраст')
-    ax3.set_ylabel('Количество пациентов')
-    ax3.set_title(f'Гистограмма распределения возраста для {selected_disease} ({gender_filter})')
-    st.pyplot(fig3)
-
-
-    @st.cache_data
-    def filter_data_disease(disease, df):
-        df_filtered = df[df.disease_name == disease]
-        return df_filtered
-
-    df_to_plot_gender = filter_data_disease(selected_disease, df_exploded)
-
-    total_men_count = df_exploded[df_exploded.gender == 'Male'].shape[0]
-    total_women_count = df_exploded[df_exploded.gender == 'Female'].shape[0]
-
-    men_count = df_to_plot_gender[(df_to_plot_gender.gender == 'Male') & (df_exploded.disease_name == selected_disease)].shape[0]
-    women_count = df_to_plot_gender[(df_to_plot_gender.gender == 'Female') & (df_exploded.disease_name == selected_disease)].shape[0]
-
-    total_count = men_count + women_count
-
-    men_ratio = men_count / total_count
-    women_ratio = women_count / total_count
-
-    plt.figure(figsize=(10, 5))
-    sns.barplot(x=['Мужчины', 'Женщины'], y=[men_ratio, women_ratio])
-
-    plt.ylim(0, 1)
-    plt.ylabel('Доля пациентов')
-    plt.xlabel('Пол')
-    plt.title(f'Доля мужчин и женщин для {selected_disease}')
-    plt.show()
-    st.pyplot(plt)
-
-    st.info("**На основе загруженных данных возможен прогноз следующих диагнозов:** " + ", ".join(top15_diseases))
-
-
 
 # Part 3: Выбор модели и параметров
 st.divider()
@@ -187,7 +108,7 @@ if uploaded_file is not None and st.session_state["dataset_name"] is not None:
         params["C"] = st.selectbox("C", [0.001, 0.01, 0.1, 1, 10, 100])
         params["class_weight"] = st.selectbox("Class Weight", [None, "balanced"])
 
-        # Параметр l1_ratio отображается только если penalty = elasticnet
+        # Параметр l1_ratio отображается, только если penalty = elasticnet
         if params["penalty"] == "elasticnet":
             params["l1_ratio"] = st.slider("L1 Ratio", 0.0, 1.0, 0.5)
         else:
@@ -195,6 +116,7 @@ if uploaded_file is not None and st.session_state["dataset_name"] is not None:
 
     # Кнопка для обучения модели
     if st.button("Обучить модель"):
+        logger.info("Запуск обучения модели %s с параметрами %s", model_type, params)
         # Формируем запрос на обучение
         train_data = {
             "model_type": model_type,
@@ -204,54 +126,68 @@ if uploaded_file is not None and st.session_state["dataset_name"] is not None:
         train_response = requests.post(f"{BACKEND_URL}/train_model", json=train_data)
 
         if train_response.status_code == 200:
+            logger.info("Модель %s успешно обучена", model_type)
             st.success("Модель успешно обучена")
             result = train_response.json()
             exp_name = result["experiment_name"]
             st.write(f"Имя эксперимента: {exp_name}")
         else:
+            logger.error("Ошибка обучения модели: %s", train_response.text)
             st.error(f"Ошибка обучения модели: {train_response.text}")
 else:
+    logger.warning("Попытка обучения модели без загрузки файла")
     st.info("Пожалуйста, загрузите файл, чтобы обучить модель.")
+
 
 # Part 4: Список моделей
 # Сохранение состояния загруженного файла для прогноза
 if "dataset_name_prediction" not in st.session_state:
     st.session_state["dataset_name_prediction"] = None
+    logger.info("Инициализировано состояние для хранения имени файла прогноза.")
 
 st.divider()
 st.subheader("Прогноз по анализу ЭКГ")
 exps_resp = requests.get(f"{BACKEND_URL}/experiments")
 if exps_resp.status_code == 200:
+    logger.info("Успешно получен список обученных моделей с сервера.")
     if "experiments" in exps_resp.json():
         exps = exps_resp.json()["experiments"]
+        logger.info(f"Найдено {len(exps)} обученных моделей.")
         df = pd.DataFrame(exps)
         df.columns = ['Model IDs']
         st.write("Список обученных моделей:")
         st.dataframe(df)
     else:
+        logger.warning("Список обученных моделей пуст.")
         st.error("Обученные модели отсутствуют.")
 
     # Выбор модели
     selected_model = st.selectbox("Выберите модель для предсказания", exps)
-    if selected_model:
-        st.write("Информация о модели и её гиперпараметры")
+    logger.info(f"Выбрана модель {selected_model} для прогноза")
 
     uploaded_file_prediction = st.file_uploader("Загрузите ZIP-файл для прогноза диагноза", type=["zip"])
     if uploaded_file_prediction is not None and st.session_state["dataset_name_prediction"] is None:
         # Отправляем файл на сервер только один раз
+        logger.info(f"Файл для прогноза загружен: {uploaded_file_prediction.name}")
         files = {"file": (uploaded_file_prediction.name, uploaded_file_prediction.read(), "text/csv")}
-        #upload_response = requests.post(f"{BACKEND_URL}/upload_dataset", files=files)
-
-        #if upload_response.status_code == 200:
-        #    st.success("Файл успешно загружен")
-        #    st.session_state["dataset_name"] = upload_response.json()["filepath"].split("/")[-1]  # Сохраняем имя файла
-        #else:
-        #    st.error(f"Ошибка загрузки файла: {upload_response.text}")
+        st.session_state["dataset_name_prediction"] = uploaded_file_prediction.name
+        disease_names = None
 
         if st.button("Получить прогноз по ЭКГ"):
-            # Формируем запрос на обучение
-            st.write("Этап обучения")
-            #result = requests.get(f"{BACKEND_URL}/predict")
-            st.info("Предсказанный диагноз: **такой-то**. Требуется консультация с врачом.")
+            logger.info("Начат процесс получения прогноза.")
+            # Заглушка для получения результата
+            upload_inference_response = requests.post(f"{BACKEND_URL}/upload_inference", json={"file": uploaded_file_prediction, "model_name": selected_model})
+
+            if upload_inference_response.status_code == 200:
+                logger.info("Модель %s успешно обучена", selected_model)
+                st.success("Модель успешно обучена")
+                result = upload_inference_response.json()
+                disease_names = result["predicts"]
+
+                logger.info("Процесс обучения завершён.")
+                st.info(f"Предсказанный диагноз: {disease_names}. Требуется консультация с врачом.")
+            else:
+                logger.error("Ошибка обучения модели: %s", upload_inference_response.text)
+                st.error(f"Ошибка обучения модели: {upload_inference_response.text}")
 else:
     st.error("Ошибка получения списка обученных моделей")
---
+++ frontend/logger_config.py
@@ -0,0 +1,15 @@
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
+log_path = os.path.join("logs", "frontend.log")
+
+if not os.path.exists("logs"):
+    os.makedirs("logs", exist_ok=True)
+
+logger = logging.getLogger("frontend_logger")
+logger.setLevel(logging.INFO)
+handler = RotatingFileHandler(log_path, maxBytes=5_000_000, backupCount=5, encoding='utf-8')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
